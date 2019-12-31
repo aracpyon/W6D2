48b1ee02e837f5a6d9c584860c99d889785ac9fe
Ara Cho <artistaracho@gmail.com>
2019-12-31 16:48:50 -0500

    W6D2 first route&controller

---
+++ first_route/app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+    def index
+        render json: User.all
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: 422
+        end
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def update
+        user = User.find(params[:id])
+        if user.update(user_params)
+            render json: user
+        else
+            render json: user.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        name = user.name
+        user.destroy
+        render json: "Successfully destroyed #{name}."
+        
+        
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
+end
